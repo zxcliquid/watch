777f341fc1af351b3b0fe0b677b350efa1226ec3
Данияр Нурмагамбетов <daniyar@MacBook-Air-Daniar.local>
2025-04-08 14:14:45 +0500

    Мой первый коммит

---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {  // Используем http-сервер
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+const users = {}; // { socketId: { username, room } }
+
+const rooms = {};
+
+io.on("connection", (socket) => {
+  console.log("Новый пользователь подключился:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push({ id: socket.id });
+    
+    socket.join(roomId);
+    io.to(roomId).emit("update-users", rooms[roomId]);
+  });
+
+  socket.on("leave-room", (roomId) => {
+    if (rooms[roomId]) {
+      rooms[roomId] = rooms[roomId].filter(user => user.id !== socket.id);
+      io.to(roomId).emit("update-users", rooms[roomId]);
+      socket.leave(roomId);
+    }
+  });
+
+  socket.on("video-action", ({ roomId, action, time }) => {  // Добавлен roomId
+    if (rooms[roomId]) {
+      io.to(roomId).emit("sync-video", { action, time });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    for (let roomId in rooms) {
+      if (rooms[roomId]) {
+        rooms[roomId] = rooms[roomId].filter(user => user.id !== socket.id);
+        io.to(roomId).emit("update-users", rooms[roomId]);
+      }
+    }
+  });
+});
+
+http.listen(5001, () => {  // Запускаем HTTP-сервер
+  console.log("Server is running on port 5001");
+});
